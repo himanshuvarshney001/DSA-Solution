c421ac3f8303ee562423ddfb88ebbc8d7404b45a
Hiamanshu Varshney <himanshuvarshney8445@gmail.com>
2025-11-20 00:25:57 +0530

    problem of the day-2

---
+++ Problem of the day leet code/Keep_multiplying_found_values_by_two.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int size;
+    int k;
+    cout<<"Enter the size of array=";
+    cin>>size;
+    vector<int> arr(size);
+    cout<<"Enter the size of array-";
+    for(int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter the number=";
+    cin>>k;
+    sort(arr.begin(),arr.end());
+    static int i=0;
+    while(1)
+    {
+        for(i=0;i<size;i++)
+        {
+            if(k==arr[i])
+            {
+                k=k*2;
+                break;
+            }
+       
+        }
+        if(i==size)
+        break;
+    }
+    cout<<k;
+}
